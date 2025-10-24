59493811906baa8b348a67d5ba7f2e9a9149858d
041041 <bhogal.san@gmail.com>
2025-10-24 16:38:16 +0530

    Fix indentation of _call_llm_variants method

--- app.py
+++ app.py
@@ -388,7 +388,7 @@ class SimpleQAWrapper:
             prompt_text = f"Question: {query}\nContext:\n{context}"
         return prompt_text, docs
 
-        def _call_llm_variants(self, prompt_text: str):
+    def _call_llm_variants(self, prompt_text: str):
         """
         Preferred call order for ChatGoogleGenerativeAI discovered from debug:
           1) invoke(prompt_str) -> returns AIMessage (has .content)
